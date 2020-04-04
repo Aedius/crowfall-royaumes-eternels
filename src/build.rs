c888ec387efb40ec73ae690d6ff7e4064413439d
Aedius <aedius.filmania@gmail.com>
2020-04-04 19:27:41 +0200

    setup header + footer + background

---
+++ src/build.rs
@@ -0,0 +1,10 @@
+use ructe::{Result, Ructe};
+
+fn main() -> Result<()> {
+    let mut ructe = Ructe::from_env()?;
+    let mut statics = ructe.statics()?;
+    statics.add_files("static")?;
+    statics.add_sass_file("scss/base.scss")?;
+
+    ructe.compile_templates("templates")
+}
