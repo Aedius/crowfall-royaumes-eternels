6a7f03becbd5e0613b9205ac3107afcb9c090a38
Aedius <aedius.filmania@gmail.com>
2020-04-06 18:03:19 +0200

    add slides

--- src/build.rs
+++ src/build.rs
@@ -5,10 +5,12 @@ fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=scss");
     println!("cargo:rerun-if-changed=scss/partials");
     println!("cargo:rerun-if-changed=templates");
+    println!("cargo:rerun-if-changed=templates/slide");
 
     let mut ructe = Ructe::from_env()?;
     let mut statics = ructe.statics()?;
     statics.add_files("static")?;
+    statics.add_files("static/slide")?;
     statics.add_sass_file("scss/base.scss")?;
 
     ructe.compile_templates("templates")
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::io::{self, Write, Error as IoError};
+use std::io::{self, Write, Error as IoError, Cursor};
 
 
 use tiny_http::{Method, Request, Response, Server, StatusCode};
@@ -8,17 +8,24 @@ include!(concat!(env!("OUT_DIR"), "/templates.rs"));
 use templates::index;
 use templates::statics::StaticFile;
 use templates::statics::favicon_ico;
-use crate::models::{Category, Config};
+
+//slide
+use templates::statics::map_jpg;
+use templates::statics::elfettes_png;
+use templates::statics::village_png;
+
+
+use crate::models::{Category, Config, Index, Slide, Article};
 
 
 fn main() {
     let server = Server::http("0.0.0.0:8000").unwrap();
 
     let mut categories = Vec::new();
-    categories.push(Category{ path: "test", name: "TEST" });
-    categories.push(Category{ path: "test 2", name: "TEST 2" });
+    categories.push(Category { path: "test", name: "TEST" });
+    categories.push(Category { path: "test 2", name: "TEST 2" });
 
-    let config = Config{
+    let config = Config {
         categories
     };
 
@@ -52,14 +59,11 @@ fn handle_get(request: Request, config: &Config) -> Result<(), IoError> {
     let url = request.url();
 
     if url == "/" {
-        let mut response = tiny_http::Response::from_string(r2s(|o| index(o, &config)));
-        let header = tiny_http::Header::from_bytes(&b"Content-Type"[..], &b"text/html; charset=utf-8"[..]).unwrap();
-        response.add_header(header);
+        let response = handle_index(&config);
         return request.respond(response);
     }
 
     if url == "/favicon.ico" {
-
         let mime_type = favicon_ico.mime.to_string();
         let mut response = tiny_http::Response::from_data(favicon_ico.content);
         let header = tiny_http::Header::from_bytes(&b"Content-Type"[..], mime_type.into_bytes()).unwrap();
@@ -67,14 +71,13 @@ fn handle_get(request: Request, config: &Config) -> Result<(), IoError> {
         return request.respond(response);
     }
 
-    let tokens:Vec<&str>= url.split("/").collect();
+    let tokens: Vec<&str> = url.split("/").collect();
 
     if tokens.len() != 3 {
         return request.respond(Response::new_empty(StatusCode(404)));
     }
 
     if let Some(data) = StaticFile::get(tokens[2]) {
-
         let mime_type = data.mime.to_string();
 
         let mut response = tiny_http::Response::from_data(data.content);
@@ -84,6 +87,36 @@ fn handle_get(request: Request, config: &Config) -> Result<(), IoError> {
     }
 
     request.respond(Response::new_empty(StatusCode(404)))
+}
+
+fn handle_index(config: &Config) -> Response<Cursor<Vec<u8>>> {
+    let data = Index {
+        slides: vec![Slide {
+            src: map_jpg.name,
+            alt: "Map",
+            url: None,
+            content: "Plusieurs cartes",
+        }, Slide {
+            src: elfettes_png.name,
+            alt: "elfettes",
+            url: None,
+            content: "oui",
+        }, Slide {
+            src: village_png.name,
+            alt: "village",
+            url: None,
+            content: "non",
+        },
+        ],
+        articles: vec![Article {
+            title: "D"
+        }],
+    };
+
+    let mut response = tiny_http::Response::from_string(r2s(|o| index(o, &config, &data)));
+    let header = tiny_http::Header::from_bytes(&b"Content-Type"[..], &b"text/html; charset=utf-8"[..]).unwrap();
+    response.add_header(header);
+    response
 }
 
 fn r2s<Call>(call: Call) -> String
@@ -97,9 +130,8 @@ fn r2s<Call>(call: Call) -> String
 
 
 mod models {
-
-    pub struct Config<'a>{
-        pub categories : Vec<Category<'a>>
+    pub struct Config<'a> {
+        pub categories: Vec<Category<'a>>
     }
 
     pub struct Category<'a> {
@@ -107,4 +139,19 @@ mod models {
         pub name: &'a str,
     }
 
+    pub struct Index<'a> {
+        pub slides: Vec<Slide<'a>>,
+        pub articles: Vec<Article<'a>>,
+    }
+
+    pub struct Slide<'a> {
+        pub src: &'a str,
+        pub alt: &'a str,
+        pub url: Option<&'a str>,
+        pub content: &'a str,
+    }
+
+    pub struct Article<'a> {
+        pub title: &'a str,
+    }
 }
